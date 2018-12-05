613583563242168557c87e2272dd76c1e6af6059
Sean Cahill <seancahill25@gmail.com>
2018-12-04 23:49:28 -0500

    Can now Scrape data and parsed into readable array

--- demo/src/main/java/com/example/demo/StockController.java
+++ demo/src/main/java/com/example/demo/StockController.java
@@ -1,9 +1,14 @@
 package com.example.demo;
 
+import java.util.ArrayList;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,10 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class StockController {
 	
 	@RequestMapping("/")
-	public static String main(String[] args) {
+	public static ArrayList<String> main(String[] args) {
 
         System.setProperty("webdriver.chrome.driver", "C:\\chromedriver_win32\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("headless");
+        options.addArguments("window-size=1366,768");
+        WebDriver driver = new ChromeDriver(options);
 
         driver.get("https://login.yahoo.com");
 
@@ -23,15 +31,30 @@ public class StockController {
         username.sendKeys("seanscrap25");
         username.submit();
         
-        
+        WebElement element = (new WebDriverWait(driver, 5))
+        		   .until(ExpectedConditions.elementToBeClickable(By.id("login-passwd")));
                 
         WebElement password = driver.findElement(By.id("login-passwd"));
         password.sendKeys("Potatosalad33");
-        password.submit();
+        password.sendKeys(Keys.ENTER);
         
         driver.get("https://finance.yahoo.com/portfolio/p_1/view/v1");
         
-
-        return "Page title is: " + driver.getTitle();        
+        WebElement element1 = (new WebDriverWait(driver, 5))
+     		   .until(ExpectedConditions.elementToBeClickable(By.id("main")));
+                
+        WebElement table = driver.findElement(By.tagName("tbody"));
+        
+        ArrayList<String> stocks = new ArrayList<String>();
+        
+        for(WebElement row: table.findElements(By.tagName("tr"))) {
+        	stocks.add(row.getText());
+        }
+        
+        driver.quit();
+       
+        return stocks;       
+        
+        
     }
 }
